e4911ef9540aa85ad157f9bba5ff25a984c1926d
윤수현 <ysh@knu.ac.kr>
2025-05-09 12:44:19 +0900

    Step1-2: Solve main task

---
+++ src/step01/HelloBiodome02.java
@@ -0,0 +1,15 @@
+package step01;
+
+public class HelloBiodome02 {
+    public static void main(String[] args) {
+        try {
+            int solar, wind, geo;
+            solar = Integer.parseInt(args[0]);
+            wind = Integer.parseInt(args[1]);
+            geo = Integer.parseInt(args[2]);
+            System.out.println("총 에너지 사용량은 " + (solar + wind + geo) + "입니다.");
+        } catch (NumberFormatException e) {
+            System.out.println("숫자를 입력해 주세요");
+        }
+    }
+}
