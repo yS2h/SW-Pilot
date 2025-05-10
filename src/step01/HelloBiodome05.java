168a3996c004371acbc4a1f0c29172f4776c8f22
sue <ysh@knu.ac.kr>
2025-05-10 13:21:46 +0900

    Step1-5: Solve main task

---
+++ src/step01/HelloBiodome05.java
@@ -0,0 +1,35 @@
+package step01;
+
+public class HelloBiodome05 {
+    public static void main(String[] args) {
+        int[] answer = find();
+        int g = answer[0];
+        int h = answer[1];
+
+        three(g, h);
+    }
+
+    public static int[] find() {
+        for (int g = 0; g < 16; g++) {
+            for (int h = 0; h < 16; h++) {
+                if (one(g, h) && two(g, h)) {
+                    return new int[]{g, h};
+                }
+            }
+        }
+        return new int[]{-1, -1};
+    }
+
+    public static boolean one(int g, int h) {
+        return (g & 1 >> g << 2 | h + g ^ h) == 1;
+    }
+
+    public static boolean two(int g, int h) {
+        return (g % 2 << h >> g | 1 & 0 ^ 0) == 2;
+    }
+
+    public static void three(int g, int h) {
+        int answer = (h * h + g) * (h << h) + (g << g);
+        System.out.println(answer);
+    }
+}
