9d25f0dbd7c0336f764e4903917ae4a8f146fd7e
윤수현 <ysh@knu.ac.kr>
2025-05-11 22:15:32 +0900

    Step1-2: Solve bonus task

--- src/step01/HelloBiodome02.java
+++ src/step01/HelloBiodome02.java
@@ -3,11 +3,13 @@ package step01;
 public class HelloBiodome02 {
     public static void main(String[] args) {
         try {
-            int solar, wind, geo;
+            int solar, wind, geo, sum;
             solar = Integer.parseInt(args[0]);
             wind = Integer.parseInt(args[1]);
             geo = Integer.parseInt(args[2]);
+            sum = solar + wind + geo;
             System.out.println("총 에너지 사용량은 " + (solar + wind + geo) + "입니다.");
+            System.out.println("태양광" + (double) solar / sum * 100 + "풍력" + (double) wind / sum * 100 + "지열" + (double) geo / sum * 100);
         } catch (NumberFormatException e) {
             System.out.println("숫자를 입력해 주세요");
         }
