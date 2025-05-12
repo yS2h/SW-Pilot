641b623549b68c83455f1fe99bd813792fd1fadb
윤수현 <ysh@knu.ac.kr>
2025-05-12 21:17:36 +0900

    Step2-2: Solve main task

---
+++ src/step02/RoadToBiodome02.java
@@ -0,0 +1,49 @@
+package step02;
+
+import java.util.ArrayList;
+
+public class RoadToBiodome02 {
+    public static void main(String[] args) {
+        String input = String.join(" ", args);
+
+        if (input.isEmpty()) {
+            System.out.println("입력된 메시지가 올바르지 않습니다. 다시 한번 확인해주세요.");
+        }
+
+        Stack s = new Stack();
+
+        for (int i = 0; i < input.length(); i++) {
+            s.push(input.charAt(i));
+        }
+
+        while (!s.isEmpty()) {
+            System.out.printf("%c", s.pop());
+        }
+    }
+
+    static class Stack {
+        private final ArrayList<Character> stack = new ArrayList<>();
+
+        public void push(char c) {
+            stack.add(c);
+        }
+
+        public char pop() {
+            if (isEmpty()) {
+                return '\0';
+            }
+            return stack.remove(stack.size() - 1);
+        }
+
+        public char peek() {
+            if (isEmpty()) {
+                return '\0';
+            }
+            return stack.get(stack.size() - 1);
+        }
+
+        public boolean isEmpty() {
+            return (stack.size() == 0);
+        }
+    }
+}
