23d9708abc813a082e56763b94fe1603085c0a43
sue <ysh@knu.ac.kr>
2025-05-17 18:06:23 +0900

    Step2-7: Fix code

--- src/step02/RoadToBiodome07.java
+++ src/step02/RoadToBiodome07.java
@@ -2,10 +2,15 @@ package step02;
 
 import java.util.*;
 
+import static java.lang.System.exit;
+
 public class RoadToBiodome07 {
 
     public static void main(String[] args) {
         Map<String, Integer> m = freq(args);
+        if (m == null) {
+            return;
+        }
         String[] animals = m.keySet().toArray(new String[0]);
 
         // 선택 정렬
@@ -37,9 +42,22 @@ public class RoadToBiodome07 {
         Map<String, Integer> m = new HashMap<>();
 
         for (String s : animals) {
+            if (isNumber(s)) {
+                System.out.println("숫자값이 아닌 동물 이름을 입력해 주세요.");
+                return null;
+            }
             m.put(s, m.getOrDefault(s, 0) + 1);
         }
 
         return m;
+    }
+
+    public static boolean isNumber(String s) {
+        try {
+            Integer.parseInt(s);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
+        }
     }
 }
