696d199a134fa444f03660b41a7c1a9595452f4f
윤수현 <ysh@knu.ac.kr>
2025-05-26 14:03:29 +0900

    Step3-6: Rename class

--- src/step03/p06/Animal.java
+++ src/step03/p06/Animal.java
@@ -1,12 +1,12 @@
-package step03;
+package step03.p06;
 
 // 앞선 과제랑 클래스 이름이 같아 임의로 Animals로 변경
-public class Animals {
+public class Animal {
     private String name;
     private String species;
     private int age;
 
-    public Animals(String name, String species, int age) {
+    public Animal(String name, String species, int age) {
         this.name = name;
         this.species = species;
         this.age = age;
--- src/step03/p06/BiodomeFamily06.java
+++ src/step03/p06/BiodomeFamily06.java
@@ -1,16 +1,16 @@
-package step03;
+package step03.p06;
 
 import java.util.LinkedList;
 
 public class BiodomeFamily06 {
     public static void main(String[] args) {
-        Animals a = new Animals("제니", "원숭이", 4);
-        Animals b = new Animals("고먀", "코끼리", 4);
-        Animals c = new Animals("타이", "호랑이", 9);
-        Animals d = new Animals("로아", "코뿔소", 5);
-        Animals e = new Animals("바비", "사슴", 7);
+        Animal a = new Animal("제니", "원숭이", 4);
+        Animal b = new Animal("고먀", "코끼리", 4);
+        Animal c = new Animal("타이", "호랑이", 9);
+        Animal d = new Animal("로아", "코뿔소", 5);
+        Animal e = new Animal("바비", "사슴", 7);
 
-        LinkedList<Animals> animals = new LinkedList<>();
+        LinkedList<Animal> animals = new LinkedList<>();
         animals.add(a);
         animals.add(b);
         animals.add(c);
@@ -20,25 +20,25 @@ public class BiodomeFamily06 {
         animalSort(animals);
     }
 
-    public static void animalSort(LinkedList<Animals> list) {
+    public static void animalSort(LinkedList<Animal> list) {
         int i1 = findIndex(list, "호랑이");
         int i2 = findIndex(list, "사슴");
         int i3 = findIndex(list, "코끼리");
 
         if (i1 > i2) {
-            Animals temp = list.remove(i2);
+            Animal temp = list.remove(i2);
             list.addLast(temp);
         }
 
         if (list.get(i3).getAge() <= 5 && i1 > i3) {
-            Animals temp = list.remove(i3);
+            Animal temp = list.remove(i3);
             list.addLast(temp);
         }
 
         print(list);
     }
 
-    public static int findIndex(LinkedList<Animals> list, String species) {
+    public static int findIndex(LinkedList<Animal> list, String species) {
         for (int i = 0; i < list.size(); i++) {
             if (list.get(i).getSpecies().equals(species)) {
                 return i;
@@ -47,10 +47,10 @@ public class BiodomeFamily06 {
         return -1;
     }
 
-    public static void print(LinkedList<Animals> list) {
+    public static void print(LinkedList<Animal> list) {
         System.out.print("[");
         for (int i = 0; i < list.size(); i++) {
-            Animals a = list.get(i);
+            Animal a = list.get(i);
             System.out.print(a.getName() + "(" + a.getSpecies() + ", " + a.getAge() + "살)");
 
             if (i != list.size() - 1) {
